effd2d0cde3ab43629bca158260ac02df0733ff7
Dave Ceddia <dceddia@gmail.com>
2017-11-15 08:58:58 -0500

    Added Provider. Still getting an error

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Counter from './Counter';
+import { Provider } from 'react-redux';
 
 const App = () => (
-  <div>
-    <Counter />
-  </div>
+  <Provider>
+    <Counter/>
+  </Provider>
 );
 
 render(<App />, document.getElementById('root'));
